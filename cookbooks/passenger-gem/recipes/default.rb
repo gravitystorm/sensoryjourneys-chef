e771e877fe5f83e50ad8ea16b19590042fa86f0e
Andy Allan <andy@gravitystorm.co.uk>
2012-06-29 13:24:48 +0100

    Add passenger cookbook.

---
+++ cookbooks/passenger-gem/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: passenger-gem
+# Recipe:: default
+#
+# Copyright 2011, Cyclestreets Ltd
+
+%w{
+    build-essential
+    libcurl4-openssl-dev
+    libssl-dev
+    zlib1g-dev
+    apache2-prefork-dev
+    libapr1-dev
+    libaprutil1-dev
+  }.each do |p|
+  package p
+end
+
+gem_package "passenger" do
+  action :install
+  version '3.0.13'
+end
+
+script "install the passenger module" do
+  interpreter "bash"
+  cwd "/tmp"
+  code <<-EOH
+    /usr/local/bin/passenger-install-apache2-module --auto
+  EOH
+  not_if "test -f /var/lib/gems/1.8/gems/passenger-3.0.13/ext/apache2/mod_passenger.so" #needs rethinking
+end
+
+template "/etc/apache2/mods-available/passenger.load" do
+  source "passenger.module.conf"
+  mode "0644"
+  notifies :restart, "service[apache2]"
+end
+
+link "/etc/apache2/mods-enabled/passenger.load" do
+  to "/etc/apache2/mods-available/passenger.load"
+  notifies :restart, "service[apache2]"
+end
